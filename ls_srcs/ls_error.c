b67176a40c8f63a7c7cf58c4dd293a6f58d4b325
Jere Rignell <jrignell@student.hive.fi>
2020-04-12 16:36:21 +0300

    updated

--- ls_srcs/ls_error.c
+++ ls_srcs/ls_error.c
@@ -6,11 +6,12 @@
 /*   By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/19 13:39:40 by jrignell          #+#    #+#             */
-/*   Updated: 2020/03/19 13:45:19 by jrignell         ###   ########.fr       */
+/*   Updated: 2020/04/12 14:21:15 by jrignell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
+#include <stdlib.h>
 
 void	ls_error(void)
 {
