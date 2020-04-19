040dea134a4e573afb7e6c88efd2d705aaab73a1
Jere Rignell <jrignell@student.hive.fi>
2020-04-19 18:14:26 +0300

    No changes

--- ls_srcs/main.c
+++ ls_srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 17:01:46 by jrignell          #+#    #+#             */
-/*   Updated: 2020/04/14 17:00:49 by jrignell         ###   ########.fr       */
+/*   Updated: 2020/04/17 21:52:18 by jrignell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
-// #include <stdio.h>
 
 static void	get_file_mode(int ac, char *av[])
 {
@@ -24,7 +23,7 @@ static void	get_file_mode(int ac, char *av[])
 	ls_get_flags(&flags, av, &i, ac);
 	av[i] ? ls_open_directories(&flags, ls_print_not_existing_f(av, &i, &flags,
 	&node), NULL, 0) : ls_print_current(&flags, &node);
-	// perror("ft_ls: ");
+	while (1);
 }
 
 int			main(int ac, char *av[])
